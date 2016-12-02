2e10d1dd37ed351a695e7d005ab3ea5f10c49e96
Luc Edixhoven <luc@edixhoven.net>
2016-12-02 12:44:53 +0100

    Removed the limit on the number of lists. Actually preventing abuse would require some more considerations.

--- backend.php
+++ backend.php
@@ -5,8 +5,6 @@ if (!defined("TinyList")) {
 	exit;
 }
 
-const MAX_LISTS = 1000;
-
 const LISTDIR = "lists/";
 const ITEMDELIM_BEGIN = "BeginItems";
 const ITEMDELIM_END = "EndItems";
@@ -61,12 +59,6 @@ function listOfLists() {
 
 // Creates a new list.
 function addList($listname, &$body) {
-	$filecount = count(scandir(LISTDIR));
-	if ($filecount >= MAX_LISTS) {
-		$body .= sprintf('<p class="note warning">Too many lists already exist. Please delete some before adding more.</p>');
-		return false;
-	}
-
 	$filename = flattenListName($listname);
 	if (!isListName($filename . '.list')) {
 		$body .= sprintf('<p class="note success">Invalid list name: %s.</p>', $listname);
